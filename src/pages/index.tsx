70b800ce9ecfccf53130d425bc8069633ba2c191
RobertoDeOliveira <robertomelo287@gmail.com>
2024-06-15 12:48:24 -0300

    install lib for carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,55 @@
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
-import camiseta1 from '../assets/1.png'
-import camiseta2 from '../assets/2.png'
-import camiseta3 from '../assets/3.png'
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
+
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef, instaceRef] = useKeenSlider({
+    slideChanged() {
+      console.log("slide changed");
+    },
+    slides: {},
+  });
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={camiseta1} width={520} height={480} alt=""/>
+    <HomeContainer ref={sliderRef}>
+      <Product className="keen-slider">
+        <Image src={camiseta1} width={520} height={480} alt="" />
         <footer>
-          <strong>
-            Camiseta X
-          </strong>
+          <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
 
-      <Product>
-        <Image src={camiseta2} width={520} height={480} alt=""/>
+      <Product className="keen-slider">
+        <Image src={camiseta2} width={520} height={480} alt="" />
         <footer>
-          <strong>
-            Camiseta X
-          </strong>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
     </HomeContainer>
-  )
+  );
 }
-  
 
 // <Image src={camiseta2} width={520} height={480} alt="" />
 //         <Image src={camiseta3} width={520} height={480} alt="" />
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -18,6 +18,13 @@ export const {
         green500: '#00875e',
         green300: '#00b37e',
 
+      },
+
+      fontSizes: {
+        md: '1.125rem',
+        lg: '1.25rem',
+        xl: '1.5rem',
+        '2xl': '2rem',
       }
     }
 })
